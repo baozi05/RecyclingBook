cbe5c2d980be014950e9991f823095c970c66998
baozi05 <1374231610@qq.com>
2025-01-28 00:18:48 +0800

    DTO类修正

--- src/main/java/org/example/baozi/recyclingbook/model/DTO/StudentInfoDTO.java
+++ src/main/java/org/example/baozi/recyclingbook/model/DTO/StudentInfoDTO.java
@@ -9,7 +9,10 @@ import javax.validation.constraints.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class StudentUpdateDTO {
+public class StudentInfoDTO {
+
+    private String studentId;
+
     @NotBlank(message = "新密码不能为空")
     @Size(min = 6,message = "密码至少需要6位")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).+$",message = "密码必须是数字和字母的组合")
@@ -17,4 +20,17 @@ public class StudentUpdateDTO {
 
     @NotBlank(message = "确认密码不能为空")
     private String confirmPassword;
+
+    @NotBlank(message = "手机号码不能为空")
+    @Pattern(regexp = "^\\d{11}$",message = "请输入正确的11位手机号")
+    private String studentPhone;
+
+    @NotBlank(message = "校区不能为空")
+    private String studentCampus;
+
+    @NotBlank(message = "宿舍地址不能为空")
+    private String studentDormitory;
+
+    @NotBlank(message = "收款方式不能为空")
+    private String payment_method;
 }
--- src/main/java/org/example/baozi/recyclingbook/model/DTO/StudentLoginDTO.java
+++ src/main/java/org/example/baozi/recyclingbook/model/DTO/StudentLoginDTO.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StudentLoginDTO {
-    @NotBlank(message = "用户名不能为空")
+    @NotBlank(message = "学号不能为空")
     private String studentId;
     @NotBlank(message = "密码不能为空")
     private String password;
